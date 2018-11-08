72683573cb790a341d3bce40fbc07088b5833445
Talbert <talbertc@usgs.gov>
2018-11-08 08:04:15 -0700

    Initial binder exploration

---
+++ install.R
@@ -0,0 +1,8 @@
+install.packages("ggplot")
+install.packages("ggmap")
+install.packages("httr")
+install.packages("rgdal")
+install.packages('xml2')
+
+install.packages('devtools')
+devtools::install_github("talbertc-usgs/nabatr")
