aed00e6926475fd29f6d87648541e05c61f13282
Enns, Kyle Douglas <kenns@usgs.gov>
2020-03-06 10:42:34 -0700

    changing acoustic report figure 4 charts

--- R/reports.R
+++ R/reports.R
@@ -743,13 +743,15 @@ build_ac_doc = function(out_dir,
     width = 850, height = 650,
     marker = list(line = list(color = 'black', width = .5)),
     color = bat_id_type, colors = c('#ff8400', '#337acc', '#23992f')) %>% # orange/blue/green
-    layout(margin = m, font = leg, xaxis = x, yaxis = y, showlegend = TRUE, autosize=F, bargap = .6,
+    layout(margin = m, font = leg, showlegend = TRUE, autosize=F, bargap = .6,
       legend = list(x = .2, y = 1.05, orientation = 'h', font = leg))
 
   # fig 2a
-  fig2_p = fig2_p_base %>% layout(title = list(x = .1, y = 1.4, text = 'Average Bat Activity Rate', font = f))
+  fig2_p = fig2_p_base %>% layout( xaxis = x, yaxis = y,
+    title = list(x = .1, y = 1.4, text = 'Average Bat Activity Rate', font = f))
   # fig 2b
-  fig2_p_log = fig2_p_base %>% layout(yaxis = y_log, title = list(x = .1, y = 1.1, text = 'Average Bat Activity Rate using a Logarithmic Scale', font = f))
+  fig2_p_log = fig2_p_base %>% layout(xaxis = x, yaxis = y_log,
+    title = list(x = .1, y = 1.1, text = 'Average Bat Activity Rate using a Logarithmic Scale', font = f))
 
   print ('Save out plotly fig2')
   # Export to a file to be used to upload into the .docx
