f0e8cca90d5de845598edf634210400057ea5918
Enns, Kyle Douglas <kenns@usgs.gov>
2020-05-12 13:08:39 -0600

    cleaning

--- install.R
+++
@@ -1,6 +0,0 @@
-install.packages(c('httr' ,'xml2' ,'jsonlite' ,'plyr' ,'rgdal' ,'sp' ,'htmltools' ,'htmlwidgets' ,
-  'leaflet' ,'rmarkdown' ,'rprojroot' ,'dplyr' ,'devtools' ,'testthat' ,'roxygen2' ,'kableExtra' ,
-  'plotly' ,'magrittr' ,'mapview' ,'officer' ,'caret' ,'plumber' ,'aws.s3' ,'flextable', 'maps',
-  'maptools'))
-devtools::install_github("ropensci/ghql")
-
