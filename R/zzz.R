e440e8090e8c87a8c03349f812ec15dbcba79701
Enns, Kyle Douglas <kenns@usgs.gov>
2020-06-12 12:18:04 -0600

    test

--- R/zzz.R
+++ R/zzz.R
@@ -5,17 +5,17 @@
   template_rmd = system.file("templates", "acoustic_stationary_report.Rmd", package = "nabatr")
   nabat_logo   = system.file("templates", "nabat_logo.png", package = "nabatr")
 
-  pkg.env = new.env()
+  pkg.env = new.env(parent = emptyenv())
   pkg.env$bats_df = NULL
   message(getwd())
-  tryCatch({
-    message('Loading in bat species range shapefiles')
-    pkg.env$species_ranges = rgdal::readOGR('./data/bat_species_ranges/')[,1:4]
-  },error = function(cond) {
-    message('Failed to load in bat species ranges')
-    message(cond)
-    return(NULL)
-  })
+  # tryCatch({
+  #   message('Loading in bat species range shapefiles')
+  #   pkg.env$species_ranges = rgdal::readOGR('./data/bat_species_ranges/')[,1:4]
+  # },error = function(cond) {
+  #   message('Failed to load in bat species ranges')
+  #   message(cond)
+  #   return(NULL)
+  # })
 
   # Shows message for path to project root
   # packageStartupMessage(template_rmd)
