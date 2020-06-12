6f8bbe46b50f91839ed113ae60b98c501b91c2a2
Enns, Kyle Douglas <kenns@usgs.gov>
2020-06-12 12:14:34 -0600

    attempting to put globals in startup of loading the package

--- R/zzz.R
+++ R/zzz.R
@@ -5,6 +5,18 @@
   template_rmd = system.file("templates", "acoustic_stationary_report.Rmd", package = "nabatr")
   nabat_logo   = system.file("templates", "nabat_logo.png", package = "nabatr")
 
+  pkg.env = new.env()
+  pkg.env$bats_df = NULL
+  message(getwd())
+  tryCatch({
+    message('Loading in bat species range shapefiles')
+    pkg.env$species_ranges = rgdal::readOGR('./data/bat_species_ranges/')[,1:4]
+  },error = function(cond) {
+    message('Failed to load in bat species ranges')
+    message(cond)
+    return(NULL)
+  })
+
   # Shows message for path to project root
   # packageStartupMessage(template_rmd)
   # packageStartupMessage(nabat_logo)
